5b924bc17b80bb39bd2e6313c2d2f5dcd564c1e5
ZixinYan <a769913449@Gamil.com>
2025-04-22 18:33:33 +0800

    update jwt

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from user.views import login, register, get_user_info, update, get_user_list, updateAvatar, findPassword, updatePwd
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     #path('/test',TestView.as_view(), name='test'),
     #path('/jwt', JwtTestView.as_view(), name='test'),
-    path('login', login, name='login'),
+    path('login', login, name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('register', register, name='register'),
     path('userlist', get_user_list, name='get_user_info'),
     path('update',update,name='update'),
